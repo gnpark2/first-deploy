c5c093450536c4caac9cf42e8194dc2bc14b208b
gnpark2 <gnpark2@gmail.com>
2025-09-25 04:42:13 +0900

    page에 resume넣기

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 // 서버 컴포넌트에서 직접 API 호출
 async function getResumeInfo() {
-  const res = await fetch('https://raw.githubusercontent.com/dMario24/first-deploy/refs/heads/main/service/resume_general_info_service.json');
+  const res = await fetch('https://raw.githubusercontent.com/gnpark2/first-deploy/refs/heads/main/service/resume_general_info_service.json');
   // API 응답이 성공적인지 확인
   if (!res.ok) {
     // 응답이 실패하면 오류를 던져 Next.js가 오류 페이지를 보여주도록 함
@@ -11,9 +11,18 @@ async function getResumeInfo() {
   return res.json();
 }
 
+async function getPortfolioInfo() {
+  const res = await fetch('https://raw.githubusercontent.com/gnpark2/first-deploy/refs/heads/main/service/resume_portfolio_service.json');
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
 export default async function Home() {
   // getResumeInfo 함수를 호출하여 데이터를 기다림
   const data = await getResumeInfo();
+  const portfolioData = await getPortfolioInfo();
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -30,11 +39,14 @@ export default async function Home() {
           <li className="mb-2 tracking-[-.01em]">
             안녕하세요{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              신입개발자 박근남입니다
-            </code>
+              신입개발자 {data.name}입니다
+              git url : {data.git_url}
+	      {data.introduction}    
+	</code>
             .
           </li>
           <li className="tracking-[-.01em]">
+            {portfolioData.title}
             열심히 해보겠습니다.
           </li>
         </ol>
