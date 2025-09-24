3b9c7dd7028b97ecf41d765e37556d1610745164
gnpark2 <gnpark2@gmail.com>
2025-09-24 16:51:32 +0900

    write api code in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
 
-export default function Home() {
+// 서버 컴포넌트에서 직접 API 호출
+async function getResumeInfo() {
+  const res = await fetch('https://raw.githubusercontent.com/dMario24/first-deploy/refs/heads/main/service/resume_general_info_service.json');
+  // API 응답이 성공적인지 확인
+  if (!res.ok) {
+    // 응답이 실패하면 오류를 던져 Next.js가 오류 페이지를 보여주도록 함
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  // getResumeInfo 함수를 호출하여 데이터를 기다림
+  const data = await getResumeInfo();
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -8,8 +22,8 @@ export default function Home() {
           className="dark:invert"
           src="/jam.jpg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={250}
+          height={65}
           priority
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
